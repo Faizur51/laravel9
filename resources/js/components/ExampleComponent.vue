a3def2f10dce05cefd1958c9f0c28552fec71d35
Faizur Rahman <61926823+Faizur51@users.noreply.github.com>
2022-12-28 14:55:26 +0600

    install vue router

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Example Component</div>
+
+                    <div class="card-body">
+                       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam amet architecto atque beatae deleniti dolor dolore eius eos facere fugit iste iure maiores mollitia necessitatibus non, nulla praesentium quae quam, ratione repellat reprehenderit sapiente sit temporibus tenetur totam ut voluptatum. Doloribus ex magni minima nostrum, obcaecati officiis qui quos ullam?
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
